7adb5c570737a47ff11bec3d004225beb4bf74cb
Gabriel Butler <gabrielbutler@Gabriels-MacBook-Pro.local>
2019-02-20 17:18:42 +0800

    Joint and marginal PMFs for the example problem 2.6-5 from the book

---
+++ 2.6 joint and marginal pmfs.R
@@ -0,0 +1,17 @@
+jpmf <- function(x1, x2){
+       (x1 + x2) / 21
+   }
+mpmf1 <- function(x1){
+       (2 * x1 + 3) / 21
+   }
+mpmf2 <- function(x2){
+       (3 * x2 + 6) / 21
+   }
+cond1 <- function(x1, x2){
+       jpmf(x1, x2) / mpmf2(x2)
+   }
+cond2 <- function(x1, x2){
+       jpmf(x1, x2) / mpmf1(x1)
+   }
+cond1(2, 2)
+cond2(2, 2)
