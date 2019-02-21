944f97def48e16fd4eab19722210cc9ece2e18a7
Gabriel Butler <gabrielbutler@Gabriels-MacBook-Pro.local>
2019-02-21 11:39:51 +0800

    Renamed functions to make them more easily recognizable

--- 2.6 joint and marginal pmfs.R
+++ 2.6 joint and marginal pmfs.R
@@ -7,9 +7,9 @@ mpmf1 <- function(x1){
 mpmf2 <- function(x2){
        (3 * x2 + 6) / 21
    }
-cond1 <- function(x1, x2){
+g <- function(x1, x2){
        jpmf(x1, x2) / mpmf2(x2)
    }
-cond2 <- function(x2, x1){
+h <- function(x2, x1){
        jpmf(x1, x2) / mpmf1(x1)
    }
