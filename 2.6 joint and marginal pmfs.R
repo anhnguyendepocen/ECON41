45c713964d9d3753886074cf0fbdde4f51bfb24c
Gabriel Butler <gabrielbutler@Gabriels-MacBook-Pro.local>
2019-02-21 11:06:15 +0800

    Updated to be more intuitive to use

--- 2.6 joint and marginal pmfs.R
+++ 2.6 joint and marginal pmfs.R
@@ -10,6 +10,6 @@ mpmf2 <- function(x2){
 cond1 <- function(x1, x2){
        jpmf(x1, x2) / mpmf2(x2)
    }
-cond2 <- function(x1, x2){
+cond2 <- function(x2, x1){
        jpmf(x1, x2) / mpmf1(x1)
    }
