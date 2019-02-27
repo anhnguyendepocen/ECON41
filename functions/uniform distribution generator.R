5b9b16cbe5ad5054e44d715fd55de57b8e95dbc3
Gabriel Butler <gabrielbutler@Gabriels-MacBook-Pro.local>
2019-02-27 21:43:43 +0800

    Tinkered with parameter names and packages to be loaded

--- functions/uniform distribution generator.R
+++ functions/uniform distribution generator.R
@@ -3,7 +3,12 @@
 # bins is number of bars to divide numbers into
 # a and b are the support (bounds) of the distribution
 uniform <- function(n, bins, a, b){
-  library(ggplot2)
-  dat <- data.frame(x = runif(n, min = min, max = max))
-  dat %>% ggplot(aes(x)) + geom_histogram(bins = bins, color = 'black', fill = 'blue') + scale_x_continuous(breaks = seq(a, b, 1))
+  library(tidyverse)
+  library(dslabs)
+  dat <- data.frame(x = runif(n, min = a, max = b))
+  dat %>% ggplot(aes(x)) + geom_histogram(bins = bins, color = 'black', fill = 'blue') + scale_x_continuous(breaks = seq(a, b, 1)) + labs(x = '', y = 'Frequency') + ds_theme_set()
 }
+
+
+
+
