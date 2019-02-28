6d6b6148990b7e283bf8f005ec3c711e6eb6c663
Gabriel Butler <gabrielbutler@Gabriels-MacBook-Pro.local>
2019-02-28 21:51:34 +0800

    Generates a plot of the normal distribution using randomly generated data for a given mean, standard deviation, number of bins/bars and number of random numbers to be generated

---
+++ functions/normal distribution generator.R
@@ -0,0 +1,6 @@
+normal <- function(n, bins, mean, sd){
+  library(tidyverse)
+  library(dslabs)
+  dat <- data.frame(x = rnorm(n, mean = mean, sd = sd))
+  dat %>% ggplot(aes(x)) + geom_histogram(aes(y = stat(count / sum(count))), bins = bins, color = 'black', fill = 'blue') + geom_vline(xintercept = mean, color = 'red', linetype = 'dashed', size = 1) + scale_x_continuous(breaks = seq(-3 * sd, 3 * sd, sd)) + labs(x = '', y = 'Relative frequency') + ds_theme_set()
+}
