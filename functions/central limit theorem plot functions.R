f8b7772c583e7c502598e6ce98b443ab2a266427
Gabriel Butler <gabrielbutler@Gabriels-MacBook-Pro.local>
2019-03-08 11:31:14 +0800

    Changed a label

--- functions/central limit theorem plot functions.R
+++ functions/central limit theorem plot functions.R
@@ -34,7 +34,7 @@ clt_plot_pois <- function(n, trials, lambda){
   mean_frame <- data.frame(x = length(0:(length(mean_data) - 1)), samp = mean_data)
   sampling_dist <- as.data.frame(scale(mean_frame)) %>% ggplot(aes(samp, stat(count / sum(count)))) + geom_histogram(color = 'black', fill = 'blue', bins = 10) + labs(x = 'Number of standard deviations', y = 'Proportion', title = 'Sampling distribution') + geom_vline(xintercept = 0, color = 'red', linetype = 'dashed', size = 1) + ds_theme_set()  
   qq <- as.data.frame(scale(mean_frame)) %>% ggplot(aes(sample = samp)) + stat_qq(color = 'blue', alpha = 0.5) + stat_qq_line() + labs(title = 'QQ plot') + ds_theme_set()
-  pdf <- dat %>% ggplot(aes(x, prob)) + geom_line(size = 1) + geom_vline(xintercept = lambda, size = 1, linetype = 'dashed', color = 'red') + scale_y_continuous(limits = c(0, max(dat$prob) + 0.05)) + labs(title = 'PDF') + ds_theme_set()
+  pdf <- dat %>% ggplot(aes(x, prob)) + geom_line(size = 1) + geom_vline(xintercept = lambda, size = 1, linetype = 'dashed', color = 'red') + scale_y_continuous(limits = c(0, max(dat$prob) + 0.05)) + labs(title = 'PMF') + ds_theme_set()
   samp_dist <- dat %>% ggplot(aes(x, prob)) + geom_bar(stat = 'identity', color = 'black', fill = 'red') + geom_vline(xintercept = lambda, size = 1, color = 'green', linetype = 'dashed') + labs(y = 'Frequency', x = 'x', title = 'Sample distribution') + ds_theme_set()
   ggarrange(pdf, samp_dist, sampling_dist, qq, ncol = 2, nrow = 2)
 }
